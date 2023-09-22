6a57d798c82d18038140ff2d16c921af6cdad94e
MiguelGarcia <mpgarcia.br@gmail.com>
2023-09-22 15:13:38 -0300

    criacao de MeuApp e das minhas urls mais basicas, ate em torno do slide 368

---
+++ MeuSite/MeuApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# Create your views here.
+
+def home(request):
+    return render(request,'MeuApp/home.html')
+
+      
+def segundaPagina(request):
+    return render(request,'MeuApp/segunda.html')
+#return HttpResponse('Alo mundo')
+        
+
