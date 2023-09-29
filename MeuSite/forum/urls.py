39a7c16a0d090868536da57840c7ca6596a47c46
MiguelGarcia <mpgarcia.br@gmail.com>
2023-09-29 14:07:46 -0300

    FORUM - listando publicacoes - ate slide 465

--- MeuSite/forum/urls.py
+++ MeuSite/forum/urls.py
@@ -1,1 +1,11 @@
-urlpatterns=[]
+from django.urls.conf import path
+from forum import views
+
+app_name = "forum"
+
+urlpatterns = [
+    path('lista/', views.PublicacaoListView.as_view(),
+        name='lista-publicacoes'),
+    path('', views.PublicacaoListView.as_view(),
+        name='home-publicacoes'),
+]
--- MeuSite/forum/views.py
+++ MeuSite/forum/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from forum.models import Publicacao
+from django.views.generic.base import View
 
-# Create your views here.
+class PublicacaoListView(View):
+    def get(self, request, *args, **kwargs):
+        publicacoes = Publicacao.objects.all()
+        contexto = { 'publicacoes': publicacoes, }
+        return render(
+                request,
+                'forum/listaPublicacoes.html',
+                contexto)
