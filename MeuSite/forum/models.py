3578e1e22d9d5507b111bb2804a55cc4b3815dcb
MiguelGarcia <mpgarcia.br@gmail.com>
2023-10-05 20:16:05 -0300

    comentarios adicionados, podendo adicionar e remover

--- MeuSite/forum/models.py
+++ MeuSite/forum/models.py
@@ -7,5 +7,16 @@ class Publicacao(models.Model):
     texto = models.CharField(
     max_length=10000, help_text='Digite o texto da publicacao')
 
-def __str__(self):
-    return self.titulo
+    def __str__(self):
+        return self.titulo
+
+
+class Comentario(models.Model):
+    id = models.AutoField(primary_key=True)
+    texto = models.CharField(
+    max_length=100, help_text='Digite o comentario')
+    idPublicacao  = models.ForeignKey(Publicacao,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.texto
+
