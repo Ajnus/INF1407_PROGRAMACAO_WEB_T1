e553cdf2a3163cc524dee725172045d741775e6b
MiguelGarcia <mpgarcia.br@gmail.com>
2023-10-06 14:46:39 -0300

    renomeando pastas

--- MeuSite/forum/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from forum.models import Publicacao,Comentario
-
-admin.site.register(Publicacao)
-admin.site.register(Comentario)
