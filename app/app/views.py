2249927ff209eee06f8af68158d55077985b9d7d
MiguelGarcia <mpgarcia.br@gmail.com>
2023-10-06 22:48:35 -0300

    cadastro com email

--- app/app/views.py
+++ app/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm
+from app.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import UpdateView
 from django.contrib.auth import login
